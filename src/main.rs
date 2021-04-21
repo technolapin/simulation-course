731296a8c5cf46b41906a3cf1e28a7859e725c47
technolapin <technolapin@orange.fr>
2021-04-21 18:01:29 +0200

    2D sim working

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ extern crate glium;
 use glium::{glutin, Surface};
 use glium::index::PrimitiveType;
 
+const SIZE_MASS_RATIO: f32 = 0.1;
 
 #[derive(Copy, Clone)]
 struct Vertex {
@@ -236,18 +237,19 @@ impl Renderer
 
 mod simu
 {
-    use nalgebra::{Vector2};
+    use nalgebra::{Vector3};
+    use rayon::prelude::*;
 
-    type Float = f64;
+    pub type Float = f64;
 
     /// Material Point
     #[derive(Default, Debug)]
     pub struct PMat
     {
         pub mass: Float,
-        pub pos: Vector2<Float>,
-        pub vel: Vector2<Float>,
-        pub forces: Vector2<Float>,
+        pub pos: Vector3<Float>,
+        pub vel: Vector3<Float>,
+        pub forces: Vector3<Float>,
         pub fixed: bool
     }
     impl PMat
@@ -257,7 +259,7 @@ mod simu
             Self
             {
                 mass,
-                pos: Vector2::new(x, y),
+                pos: Vector3::new(x, y, 0.),
                 .. Default::default()
             }
         }
@@ -293,13 +295,9 @@ mod simu
             l: Float,
             z: Float
         },
-        Unconditional
-        {
-            s: Float
-        },
         FrcConst
         {
-            frc: Vector2<Float>
+            frc: Vector3<Float>
         },
         SpringBrakeBreakable
         {
@@ -314,7 +312,7 @@ mod simu
 
     impl Link
     {
-        fn setup(&mut self, a: &PMat, b: &PMat) -> Vector2<Float>
+        fn setup(&mut self, a: &PMat, b: &PMat) -> Vector3<Float>
         {
             use Link::*;
             
@@ -336,7 +334,7 @@ mod simu
                     }
                     else
                     {
-                        Vector2::new(0.0, 0.0)
+                        Vector3::new(0.0, 0.0, 0.0)
                     }
                 },
                 SpringTreshOut{k, l, s} =>
@@ -349,7 +347,7 @@ mod simu
                     }
                     else
                     {
-                        Vector2::new(0.0, 0.0)
+                        Vector3::new(0.0, 0.0, 0.0)
                     }
                 },
                 Brake{z} =>
@@ -363,10 +361,6 @@ mod simu
 
                     k*(d-l*dir) + z*(b.vel - a.vel)
                 },
-                Unconditional{s} =>
-                {
-                    unimplemented!()
-                },
                 FrcConst{frc} =>
                 {
                     frc
@@ -379,7 +373,7 @@ mod simu
                         if d.norm() > s
                         {
                             active = false;
-                            return Vector2::new(0.0, 0.0);
+                            return Vector3::new(0.0, 0.0, 0.0);
                         }
                         let dir = d.normalize();
                         
@@ -388,7 +382,7 @@ mod simu
                     }
                     else
                     {
-                        Vector2::new(0.0, 0.0)
+                        Vector3::new(0.0, 0.0, 0.0)
                     }
                 }
             }
@@ -427,7 +421,7 @@ mod simu
         {
             for pt in self.points.iter_mut()
             {
-                pt.forces = Vector2::new(0.0, 0.0);
+                pt.forces = Vector3::new(0.0, 0.0, 0.0);
             }
         }
         
@@ -481,15 +475,15 @@ mod simu
 
         pub fn make_scene(&self) -> crate::Scene
         {
-            let mut primitives = vec![];
 
-            for pt in self.points.iter()
+            let primitives = self.points.par_iter().map(|pt|
             {
                 let x = pt.pos[0] as f32;
                 let y = pt.pos[1] as f32;
-                let r = pt.mass as f32;
-                primitives.push(crate::Primitive::Disk{x, y, r});
-            }
+                let r = (pt.mass as f32)*crate::SIZE_MASS_RATIO;
+
+                crate::Primitive::Disk{x, y, r}
+            }).collect();
 
             crate::Scene(primitives)
             
@@ -508,68 +502,238 @@ mod simu
 
 
 use std::collections::HashMap;
-struct Scene(Vec<Primitive>);
+pub struct Scene(pub Vec<Primitive>);
 
-enum Primitive
+pub enum Primitive
 {
     Disk{x: f32, y: f32, r: f32},
     Line{x1: f32, y1: f32, x2: f32, y2: f32}
 }
 
 
+use crate::simu::{World, PMat, Link, Float};
+
+// width and height are nb of cells
+fn build_2D_grid(world: &mut World, w: usize, h: usize)
+{
+    let pas = 1.0/ (w.max(h) as Float);
+    let cx = w as Float * pas / 2.0;
+    let cy = h as Float * pas / 2.0;
+
+    let pos2index = |i: usize, j: usize| i + j*w;
+    let index2pos = |index: usize| (index % w, index / w);
+
+    let pos2real = |i: usize, j: usize| ((i as Float)*pas - cx, (j as Float)*pas - cy);
+
+    
+    let mass = 0.1;
+
+
+    // points
+    for j in 0..h
+    {
+        for i in 0..w
+        {
+            let (x, y) = pos2real(i, j);
+
+            world.add_point(
+                PMat::new(mass, x, y)
+            );
+        }
+    }
+
+
+    // structure
+    let k = 1000.;
+    let l = pas;
+    let z = 0.9;
+
+    // structure horizontal
+    for j in 0..h
+    {
+        for i in 0..(w-1)
+        {
+            let index0 = pos2index(i, j);
+            let index1 = pos2index(i+1, j);
+            world.add_link(
+                Link::SpringBrake{k, l, z},
+                index0,
+                index1
+            );
+        }
+    }
+    // structure vertical
+    for j in 0..(h-1)
+    {
+        for i in 0..w
+        {
+            let index0 = pos2index(i, j);
+            let index1 = pos2index(i, j+1);
+            world.add_link(
+                Link::SpringBrake{k, l, z},
+                index0,
+                index1
+            );
+        }
+    }
+
+    // courbure
+    let k = 200.;
+    let l = pas*2.;
+    let z = 0.2;
+
+    // courbure horizontal
+    for i in 0..(w-2)
+    {
+        for j in 0..h
+        {
+            let index0 = pos2index(i, j);
+            let index1 = pos2index(i+2, j);
+            world.add_link(
+                Link::SpringBrake{k, l, z},
+                index0,
+                index1 // right
+            );
+        }
+    }
+    // courbure vertical
+    for i in 0..w
+    {
+        for j in 0..(h-2)
+        {
+            let index0 = pos2index(i, j);
+            let index1 = pos2index(i, j+2);
+            world.add_link(
+                Link::SpringBrake{k, l, z},
+                index0,
+                index1 // right
+            );
+        }
+    }
+
+    // cisaillement
+    let k = 200.;
+    let l = pas*(2.0f32.sqrt() as Float);
+    let z = 0.2;
+
+    for i in 0..(w-1)
+    {
+        for j in 0..(h-1)
+        {
+            // a b
+            // c d    
+            let a = pos2index(i, j);
+            let b = pos2index(i+1, j);
+            let c = pos2index(i, j+1);
+            let d = pos2index(i+1, j+1);
+            
+            world.add_link(
+                Link::SpringBrake{k, l, z},
+                a,
+                d // upleft to downright
+            );
+            world.add_link(
+                Link::SpringBrake{k, l, z},
+                b,
+                c // upright to downleft
+            );
+        }
+    }
+
+    for index in 0..(w*h)
+    {
+        world.add_link(
+            Link::FrcConst{frc: nalgebra::Vector3::new(0.0, -1.0*mass, 0.0)},
+            index, index
+        );
+    }
+/*
+    // points du haut fixes
+    for i in 0..w
+    {
+        let index = pos2index(i, 0);
+        world.points[index].fixed = true;
+
+    }
+  */  
+    // points de gauche fixes
+    for j in 0..h
+    {
+        let index = pos2index(0, j);
+        world.points[index].fixed = true;
+
+    }
+    
+    
+}
+
 fn main() {
     use crate::simu::{World, PMat, Link};
     let mut world = World::new();
 
-    let nb_points = 100;
-    let zoom = 0.2;
-    let rope_len = 1.0*zoom;
-    let mass = 0.1;
-    let pas = rope_len/((nb_points-1) as f64);
-    world.add_point(PMat::new(mass, -rope_len/2.0, 0.0));
 
-    
-    
-    for i in 0..nb_points
+    if false
     {
-        world.add_point(PMat::new(mass, ((i+1) as f64)*pas-rope_len/2.0, 0.0));
+        let nb_points = 1000;
+        let zoom = 0.2;
+        let rope_len = 1.0*zoom;
+        let mass = 0.01;
+        let pas = rope_len/((nb_points-1) as f64);
+        let y = 0.5;
+        world.add_point(PMat::new(mass, -rope_len/2.0, y));
+
+        let g = 1.0;
+        let k = 2000f64;
+        let l = pas;
+        let z = 0.9;
+        for i in 0..nb_points
+        {
+            world.add_point(PMat::new(mass, ((i+1) as f64)*pas-rope_len/2.0, y));
+
+            world.add_link(
+                Link::SpringBrake{k, l, z},
+                i,
+                i+1
+            );
+        }
+
+        let k = 500f64;
+        let l = pas*2.;
+        let z = 0.9;
+
+        for i in 0..nb_points-1
+        {
+            world.add_link(
+                Link::SpringBrake{k, l, z},
+                i,
+                i+2
+            );
+        }
 
         
-        world.add_link(
-            Link::SpringBrake{k: 1.0, l: pas, z: 0.1},
-            i,
-            i+1
-        );
-
-        world.add_link(
-            Link::SpringTreshOut{k: 4.0, l: pas*1.5, s: pas*1.5},
-            i,
-            i+1
-        );
-        world.add_link(
-            Link::SpringTreshIn{k:  10., l: pas*0.5, s: pas*0.5},
-            i,
-            i+1
-        );
+        world.points[0].fixed = true;
+        world.points[nb_points].fixed = true;
+        
+        //world.points[5].pos[1] = 0.3;
+        for i in 0..world.points.len()
+        {
+            world.add_link(
+                Link::FrcConst{frc: nalgebra::Vector3::new(0.0, -1.0*g*mass, 0.0)},
+                i, i
+            );
+        }
+    }
+    else
+    {
+        build_2D_grid(&mut world, 100, 100);
 
     }
 
-    world.points[0].fixed = true;
-    world.points[nb_points].fixed = true;
-    
-    //world.points[5].pos[1] = 0.3;
     
     let event_loop = glutin::event_loop::EventLoop::new();
 
     let mut renderer = Renderer::new(&event_loop);
 
-    for i in 0..world.points.len()
-    {
-        world.add_link(
-            Link::FrcConst{frc: nalgebra::Vector2::new(0.0, -0.002)},
-            i, i
-        );
-    }
 
     /*
     let vertices = vec![
@@ -613,30 +777,52 @@ fn main() {
         },
     ).unwrap();
 
-    // Here we draw the black background and triangle to the screen using the previously
-    // initialised resources.
-    //
-    // In this case we use a closure for simplicity, however keep in mind that most serious
-    // applications should probably use a function that takes the resources as an argument.
-
     let palette = vec![obj];
 
     use std::time::Instant;
     
-    let mut date_prev_frame = Instant::now();
-        
+    let mut date_prev_tick = Instant::now();
 
+
+    let f_ech = 10000; //Hz (= tps)
+    let fps = 600; //Hz (= fps)
+    let dt = 1./(f_ech as f64);
+
+    let mut tick_count = 0;
+    let mut tps_probe_date = Instant::now();
+    let tps_probe_resolution = 1000000;
+
+    let mut actual_tps = 0;
+    
     // the main loop
     event_loop.run(move |event, _, control_flow| {
 
-        if date_prev_frame.elapsed().as_millis() >= 1000/60
-        {
-            world.run(0.1);
-            let scene = world.make_scene();
-            renderer.draw_scene(scene, &palette, &program);
-            renderer.swap();
-            date_prev_frame = Instant::now();
 
+        
+        if date_prev_tick.elapsed().as_micros() >= 1000000/f_ech
+        { 
+            world.run(dt);
+            date_prev_tick = Instant::now();
+            tick_count += 1;
+
+
+
+            if tps_probe_date.elapsed().as_micros() >= tps_probe_resolution
+            {
+                actual_tps = tick_count*1000000 / tps_probe_resolution;
+                tps_probe_date = Instant::now();
+                tick_count = 0;
+                println!("TPS: {}", actual_tps);
+            }
+
+
+            if tick_count % (f_ech/fps) == 0
+            {
+                let scene = world.make_scene();
+                renderer.draw_scene(scene, &palette, &program);
+                renderer.swap();
+            }
+            
         }
 
         
@@ -653,12 +839,12 @@ fn main() {
             },
             glutin::event::Event::DeviceEvent{event, ..} =>
             {
-                println!("{:?}", event);
+                //println!("{:?}", event);
                 match event
                 {
                     glutin::event::DeviceEvent::Key{..} =>
                     {
-                        world.points[0].fixed = false;
+                        world.points[0].fixed = !world.points[0].fixed;
                     },
                     
                     _ => ()
